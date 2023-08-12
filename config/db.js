585b626fd4013aa963bed4478edb4c38609a5d0c
AntonovAtanas <atanas.antonov1@gmail.com>
2023-08-12 20:45:55 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/screenia'
-// const uri = 'mongodb+srv://atanasantonov1:aJWBoLpp4jckWp6G@screenia.hmyhgfr.mongodb.net/?retryWrites=true&w=majority'
+// const uri = 'mongodb://127.0.0.1:27017/screenia'
+const uri = 'mongodb+srv://atanasantonov1:aJWBoLpp4jckWp6G@screenia.hmyhgfr.mongodb.net/?retryWrites=true&w=majority'
 
 async function dbConnect () {
   try {
